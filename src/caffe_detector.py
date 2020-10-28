7b0584f86189d15d679444c39cfeab5823e5524d
ihor_acer <igorko323@gmail.com>
2020-10-28 22:58:03 +0200

    add caffe face detector

---
+++ src/caffe_detector.py
@@ -0,0 +1,54 @@
+from imutils.video import VideoStream
+import numpy as np
+import imutils
+import time
+import cv2
+
+
+def run():
+    threshold = 0.5
+    prototxt = "../data/deploy.prototxt"
+    model_path = "../data/res10_300x300_ssd_iter_140000.caffemodel"
+    net = cv2.dnn.readNetFromCaffe(prototxt, model_path)
+
+    vs = VideoStream(src=0).start()
+    time.sleep(2.0)
+
+    while True:
+        frame = vs.read()
+        frame = imutils.resize(frame, width=800)
+
+        h, w = frame.shape[:2]
+
+        blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
+                                     (300, 300), (104.0, 177.0, 123.0))
+
+        net.setInput(blob)
+        detections = net.forward()
+
+        for i in range(detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+
+            if confidence < threshold:
+                continue
+
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            startX, startY, endX, endY = box.astype("int")
+
+            text = "{:2f}%".format(confidence * 100)
+            y = startY - 10 if startY > 20 else startY + 10
+            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
+            cv2.putText(frame, text, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        if key == ord("q"):
+            break
+
+    cv2.destroyAllWindows()
+    vs.stop()
+
+
+if __name__ == "__main__":
+    run()
